16c8cd5d3e79e78b6a034e5a89b01849439e05bc
Daksh Kulshreshtha <akshd@outlook.com>
2021-07-21 00:31:42 +0530

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Dashnode</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Hero />
     </div>
   );
 }
